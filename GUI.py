4437b6a6331389ec72054f9b57e02683a3baa54d
Purushoth Uzumaki <100405616+purushothaman7@users.noreply.github.com>
2024-12-19 21:08:11 +0530

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -4,6 +4,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
+import os
 
 app = Flask(__name__)
 
@@ -82,4 +83,5 @@ def predict():
         return render_template('index.html', error=str(e))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Get the PORT from environment or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
